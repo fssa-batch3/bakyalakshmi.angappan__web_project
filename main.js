b5adf6ddd9df2cd6a623a177b625ed06d8b24578
Bakyalakshmi <116268707+bakya236@users.noreply.github.com>
2023-03-27 22:32:27 +0530

    feat: added local datas

--- main.js
+++ main.js
@@ -5,12 +5,12 @@ let gender_data = [{"gender":"men","id":1},{"gender":"women","id":2},{"gender":"
 let size_data = [{"value":"s","quantity":12,"status":true,"id":1},{"value":"l","quantity":12,"status":true,"id":2},{"value":"m","quantity":12,"status":true,"id":3},{"value":"xl","quantity":12,"status":true,"id":4},{"value":"xxl","quantity":12,"status":true,"id":5},{"value":"32","quantity":12,"status":true,"id":6},{"value":"34","quantity":12,"status":true,"id":7},{"value":"36","quantity":12,"status":true,"id":8},{"value":"38","quantity":12,"status":true,"id":9},{"value":"40","quantity":12,"status":true,"id":10}]
 
 
-if (!gender_data) {
+if (!gender_list) {
     localStorage.setItem("product_list"),JSON.stringify(gender_data);
 }
 
 
-if (!size_data) {
+if (!size_list) {
     localStorage.setItem("product_list"),JSON.stringify(size_data);
 }
 
